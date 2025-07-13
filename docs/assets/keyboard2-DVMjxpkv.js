b776198b0e03d584b19e2b5fc8ccaf91b0edcee1
Elysium <elysium@i-e.space>
2025-07-13 21:19:56 +0300

    :fix pages

--- docs/assets/keyboard2-DVMjxpkv.js
+++ docs/assets/keyboard2-DVMjxpkv.js
@@ -1,4 +1,4 @@
-import{w as t}from"./index-CN6VJFlh.js";/*!
+import{w as t}from"./index-BvvM_A5q.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const r=()=>{if(t!==void 0)return t.Capacitor};/*!
  * (C) Ionic http://ionicframework.com - MIT License
